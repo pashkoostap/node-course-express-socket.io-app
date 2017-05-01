dff6fbd89df8004c275432c9566f1ef8fd45ab3b
pashkoostap <pashko.ostap@gmail.com>
2017-05-01 18:41:49 +0300

    created npm start command for heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(publicPath));
 

1d7244dd1e1f679f2c276ae5ae0164a39390834c
pashkoostap <pashko.ostap@gmail.com>
2017-05-01 23:14:01 +0300

    created form for sending a message to server

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,33 @@
 let socket = io();
+let form = document.getElementById('message-form');
+let inputField = form.querySelector('.form-control');
+let messagesList = document.querySelector('.messages-list');
 
 socket.on('connect', () => {
   console.info('connected');
-
-  // socket.emit('createMessage', {
-  //   from: 'user 1',
-  //   text: 'Hello',
-  //   createdAt: new Date().getTime()
-  // })
 });
 
 socket.on('disconnect', () => {
   console.info('disconnected');
 });
 
-socket.on('newMessage', (message) => { 
+socket.on('newMessage', (message) => {
   console.log('newMessage', message);
+  let newElement = document.createElement('li');
+  newElement.innerText = `${message.from}: ${message.text}`;
+  messagesList.appendChild(newElement);
 });
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let messageText = inputField.value;
+  if (messageText) {
+    socket.emit('createMessage', {
+      from: 'User',
+      text: messageText
+    }, (data) => {
+      console.log('Got it', data);
+    });
+    inputField.value = '';
+  }
+})
